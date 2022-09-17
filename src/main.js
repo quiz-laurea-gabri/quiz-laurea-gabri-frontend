ec9d377017c4d90a336e12feaf39f1bb3caa42f3
Ledmington <ledmington.dev@gmail.com>
2022-09-17 17:35:23 +0200

    wip

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
+import axios from 'axios'
 import { createApp } from 'vue'
 import App from './App.vue'
 
 import './assets/main.css'
 
 createApp(App).mount('#app')
+
+axios.get("https://quiz-laurea-gabri.herokuapp.com/").then(res => console.log("Received " + res))
